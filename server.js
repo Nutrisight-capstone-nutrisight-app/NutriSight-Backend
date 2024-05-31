419d9b2289252d58ceec342ad3bc314eac6f69ff
tsalisa <tsalisa.syifa.afia-2021@fst.unair.ac.id>
2024-05-31 22:07:46 +0700

    make routers

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 3000;
+import { route } from "./routes/users.js";
 
 app.get("/", (req, res) => {
   res.send("Request Success");
@@ -9,3 +10,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.use("/users", route);
