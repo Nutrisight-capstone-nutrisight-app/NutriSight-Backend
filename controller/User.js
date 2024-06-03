4ac4ba0bcd51ccb75d87398ffc711cc5622c6b47
tsalisa <tsalisa.syifa.afia-2021@fst.unair.ac.id>
2024-06-03 15:30:24 +0700

    add hash on password

--- controller/User.js
+++ controller/User.js
@@ -1,4 +1,5 @@
 import prisma from "../prisma/client.js";
+import bcrypt from "bcrypt";
 
 export const getUserById = async (req, res) => {
   const id = req.params.id;
@@ -11,6 +12,26 @@ export const getUserById = async (req, res) => {
 };
 
 export const createUser = async (req, res) => {
+  if (res.body?.name === undefined) {
+    return res.status(400).send("Please insert name");
+  }
+  if (res.body?.email === undefined) {
+    return res.status(400).send("Please insert email");
+  }
+  if (res.body?.usename === undefined) {
+    return res.status(400).send("Please insert username");
+  }
+  if (res.body?.password === undefined) {
+    return res.status(400).send("Please insert password");
+  }
+
+  try {
+    const saltRound = 10;
+    const password = await bcrypt.hash(req.body.password, saltRound);
+    req.body.password = password;
+  } catch (error) {
+    console.error("Error hashing password:", error);
+  }
   const user = req.body;
   const createUser = await prisma.user.create({
     data: user,
